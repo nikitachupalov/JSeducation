8853b6fe44d1d811d9426a53335070af690480f5
Nikita <chupalovn@gmail.com>
2022-02-19 23:23:03 +0300

    My first JS website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-alert("Нажми 'к'");
+const numberOffilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+
+
+ const personalMovieDB = {
+    count : numberOffilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+const a = prompt('Один из просмотренныз фильмов', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренныз фильмов', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+                         
+
+
+
