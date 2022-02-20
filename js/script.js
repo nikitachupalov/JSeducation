f993046b97d37d57e04b60065549a6804dbfc943
Nikita <chupalovn@gmail.com>
2022-02-21 02:13:59 +0300

    Objects.1 part.Learning.

--- js/script.js
+++ js/script.js
@@ -1,95 +1,57 @@
-/* Задание на урок:
+"use strict";
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-'use strict';
-
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const options = {
+    name:'test',
+    width : 1024,
+    length : 1024,
+    color : {
+        border:'black',
+        bg:'red'
+    },
+    makeTest: function () {
+        console.log('Test');
     }
-}
-
-start();
-
-let privat;
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat : false
 };
 
 
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
+options.makeTest();
 
-rememberMyFilms();
+
+const { border, bg} = options.color;
+
+console.log(border);
+
+
+console.log(Object.keys(options).length);
 
 
 
+console.log(options.name);
 
-function detectPersonalLevel(){
-    
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
+
+delete options.name;
+
+
+console.log(options);
+
+let counter = 0;
+for (let key in options){
+    if (typeof(options[key]) === 'object' ) {
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        } 
     } else {
-        console.log("Произошла ошибка");
-    }
-
-}
-
-detectPersonalLevel();
-
-
-function writeYourGenres(){
-    for( let i = 1; i < 4; i++) {
-        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}.`));
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
     }
 }
 
 
-writeYourGenres();
 
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
 
-showMyDB(personalMovieDB.privat);
+
+
+
 
 
 
