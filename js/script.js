bc7e700c93f9f95d6490decc415919aab99fc4d5
Nikita <chupalovn@gmail.com>
2022-02-21 22:18:39 +0300

    OOP.Part 1.Learning

--- js/script.js
+++ js/script.js
@@ -1,85 +1,9 @@
-"use strict";
-// let a = 5,
-//     b = a;
+'use strict';
 
-// b = b + 5;
+// let str = 'Some';
+// let strObj = new String(str);
 
-// console.log(b, a);
-
-// const obj = {
-//     a : 5,
-//     b : 1
-
-// };
-
-// const copy = obj;
-// copy.a = 10;
-// console.log(copy);
-// console.log(obj);
+// console.log(typeof(strObj));
 
 
-
-function copy(mainObj){
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj){
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a : 2,
-    b : 5,
-    c : {
-        d : 4,
-        e : 7
-    }
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-
-console.log(newNumbers);
-console.log(numbers);
-
-
-const add = {
-    d : 17,
-    e : 20
-};
-
-
-
-
-const clon = Object.assign({},add);
-
-clon.e = 22;
-
-// console.log(clon);
-// console.log(add);
-
-const oldArray = ['a', 'b', 'c'];
-
-const newArray = oldArray.slice();
-
-newArray[1] = 'd';
-
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo','rutube'],
-      blogs = ['wordpress' , 'livejournal'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log (a, b, c){
-    console.log(a);
-}
-
-
-
+console.dir([1,2,3]);
