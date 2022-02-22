ddcf5ca059b2cd5e04e61b8b340afc018fb5d092
Nikita <chupalovn@gmail.com>
2022-02-22 20:55:58 +0300

    Dynamic type.Part 1.Learning

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 'use strict';
 
-// let str = 'Some';
-// let strObj = new String(str);
 
-// console.log(typeof(strObj));
+console.log(typeof(String(null)));
+
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
 
 
-console.dir([1,2,3]);
+const fontSize = 26+'px';
+
+
+console.log(typeof(Number('4')));
+
+console.log(typeof(+'5'));
+
+
+// to boolean
+
+// 0,'',null,undefined,Nan; - false
+
+let switcher = null;
+if(switcher){
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if(switcher){
+    console.log('Working1...');
+}
+
+
